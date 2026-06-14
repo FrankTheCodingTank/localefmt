8fe62c70920cea5784a9f43374ab24ea843a4626
FrankTheCodingTank <0xmoose@bt6.gg>
2026-06-14 11:26:33 -0700

    Revert added date regression tests

--- localefmt/tests/date.test.js
+++
@@ -1,9 +0,0 @@
-import assert from 'node:assert/strict';
-import { formatDate } from '../src/date.js';
-
-assert.equal(formatDate('1990-04-17', 'en-US'), 'April 17, 1990');
-assert.equal(formatDate('1990-04-17', 'en-CA', false), '1990-04-17');
-assert.equal(formatDate('1990-04-17', 'en-GB'), '17 April 1990');
-assert.equal(formatDate('1990-04-17', 'en-AU', false), '17/04/1990');
-
-console.log('Date tests passed');
